497e042c263571d57485592c8336d9f6be9bafc0
gamari <gamari4552@gmail.com>
2022-06-30 17:38:29 +0900

    プログレスバーの追加。

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,16 @@ import "../../styles/globals.css";
 import type { AppProps } from "next/app";
 import { AnimatePresence } from "framer-motion";
 
+import NextNProgress from "nextjs-progressbar";
+
 function MyApp({ Component, pageProps, router }: AppProps) {
   return (
-    <AnimatePresence exitBeforeEnter>
-      <Component key={router.asPath} {...pageProps} />
-    </AnimatePresence>
+    <>
+      <NextNProgress />
+      <AnimatePresence exitBeforeEnter>
+        <Component key={router.asPath} {...pageProps} />
+      </AnimatePresence>
+    </>
   );
 }
 
